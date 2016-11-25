e0ba2ff9064b43e86971d2dc434be7b16b1ee5ba
konojunya <junya03dance@gmail.com>
2016-11-26 05:43:11 +0900

    無駄なフォルダ消す

--- trash/public/javascripts/src/admin.js
+++
@@ -1,6 +0,0 @@
-(function($){
-  var socket = io("http://" + host + ":" + port);
-  $(".btn").on("click", function() {
-    socket.emit("reload","hoge");
-  });
-})(jQuery);
--- trash/public/javascripts/src/settings.js
+++
@@ -1,4 +0,0 @@
-(function(global) {
-    global.host = window.location.hostname;
-    global.port = window.location.port;
-})(window);
